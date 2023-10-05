0a8fc3cb7210b7a86ae203506cc8ebeb68b93c71
TheRealSuki <sraglid2001@gmail.com>
2023-10-06 00:02:44 +0100

    Initial setup.

---
+++ testing.py
@@ -0,0 +1,5 @@
+import pymunk               # Import pymunk..
+
+# To run example simulations use console - python3 -m pymunk.examples -l
+
+print("This is gonna be the littest project ever :fire: :fire: :fire:")
